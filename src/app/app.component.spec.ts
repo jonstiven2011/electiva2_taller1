d2b6f74b1b86ab369ae31a0929982ff3d320e7ca
jonstiven2011 <jonstiven2011@hotmail.com>
2016-11-15 01:35:05 -0500

    taller1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,7 +3,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('App: Electiva2Taller1', () => {
+describe('App: Electiva2', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
